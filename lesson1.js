3c79d7c7ec2ad2087cfa58138c14c8a6b2433421
EvgeniyTuinov <47924160+EvgeniyTuinov@users.noreply.github.com>
2020-11-09 11:15:13 +0300

    lesson1.1

--- lesson1.js
+++ lesson1.js
@@ -11,25 +11,25 @@ cursor
     .write('\n')           // And a final \n to wrap things up
 
 // Rendering modes are persistent:
-cursor.hex('#660000').bold().underline()
+cursor.hex('#660000').bold().underline();
 
 // You can use the regular logging functions, text will be green:
-console.log('This is blood red, bold text')
+console.log('This is blood red, bold text');
 
 // To reset just the foreground color:
-cursor.fg.reset()
+cursor.fg.reset();
 
-console.log('This will still be bold')
+console.log('This will still be bold');
 
 // to go to a location (x,y) on the console
 // note: 1-indexed, not 0-indexed:
-cursor.goto(10, 5).write('Five down, ten over')
+cursor.goto(10, 5).write('Five down, ten over');
 
 // to clear the current line:
-cursor.horizontalAbsolute(0).eraseLine().write('Starting again')
+cursor.horizontalAbsolute(0).eraseLine().write('Starting again');
 
 // to go to a different column on the current line:
-cursor.horizontalAbsolute(5).write('column five')
+cursor.horizontalAbsolute(5).write('column five');
 
 // Clean up after yourself!
-cursor.reset()
+cursor.reset();
